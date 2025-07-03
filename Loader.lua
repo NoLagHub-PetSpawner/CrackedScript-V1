f5587bec2aeb8d02be41c49a54b1faa5e86efadd
NoLagHub-PetSpawner <lxlawliet0@gmail.com>
2025-07-04 00:43:49 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLagHub-PetSpawner/Handler/refs/heads/main/17cfae2e620ab9b88ad2b87218e7cceb.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLagHub-PetSpawner/Handler/refs/heads/main/df75aa195083802b274b6dbeab970928.txt"))()
